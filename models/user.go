ef15d30346ddf992a2ca03e7acf66a065555a687
Andrea Molinari <molinari@live.it>
2021-09-24 17:19:35 +0200

    feat: :construction_worker: Iniziamo a studiare come i cani

---
+++ models/user.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID       int
+	Name     string
+	Email    string
+	Password string
+}
+
+//create a user
+func CreateUser(db *gorm.DB, User *User) (err error) {
+	err = db.Create(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//get users
+func GetUsers(db *gorm.DB, User *[]User) (err error) {
+	err = db.Find(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//get user by id
+func GetUser(db *gorm.DB, User *User, id string) (err error) {
+	err = db.Where("id = ?", id).First(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//get user by id
+func Login(db *gorm.DB, User *User, loginRequest *User) (err error) {
+	err = db.Where("email = ?", loginRequest.Email).Where("password = ?", loginRequest.Password).First(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//update user
+func UpdateUser(db *gorm.DB, User *User) (err error) {
+	db.Save(User)
+	return nil
+}
+
+//delete user
+func DeleteUser(db *gorm.DB, User *User, id string) (err error) {
+	db.Where("id = ?", id).Delete(User)
+	return nil
+}
